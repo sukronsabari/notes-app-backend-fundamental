1db8e2a5ff8fe247563de8adc2dac5efa719c657
sukronsabari11 <sukronsabari11@gmail.com>
2023-05-02 14:12:41 +0800

    user registration implemented

---
+++ src/api/users/handler.js
@@ -0,0 +1,90 @@
+const ClientError = require("../../exceptions/ClientError");
+
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postUserHandler = this.postUserHandler.bind(this);
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
+  }
+
+  async postUserHandler(req, h) {
+    try {
+      this._validator.validateUserPayload(req.payload);
+
+      const { username, password, fullname } = req.payload;
+
+      const userId = await this._service.addUser({
+        username,
+        password,
+        fullname,
+      });
+
+      const response = h.response({
+        status: "success",
+        message: "User berhasil ditambahkan",
+        data: {
+          userId,
+        },
+      });
+
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+
+        response.code(error.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: "fail",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+
+      response.code(500);
+      console.log(error);
+      return response;
+    }
+  }
+
+  async getUserByIdHandler(req, h) {
+    try {
+      const { id } = req.params;
+      const user = await this._service.getUserById(id);
+
+      return {
+        status: "success",
+        data: {
+          user,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+
+        response.code(error.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: "fail",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+
+      response.code(500);
+      console.log(error);
+      return response;
+    }
+  }
+}
+
+module.exports = UsersHandler;
--- src/server.js
+++ src/server.js
@@ -3,11 +3,16 @@ require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
 const notes = require("./api/notes");
+const users = require("./api/users");
 const NotesService = require("./services/postgres/NotesService");
 const NotesValidator = require("./validator/notes");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -18,13 +23,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
